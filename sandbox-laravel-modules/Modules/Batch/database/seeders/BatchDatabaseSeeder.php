6d988aa93e5a2a92c4261c9a970e21a4a67e5bfe
oshiro-kazuma <kazuma.ooshiro@gmail.com>
2025-01-11 15:03:44 +0900

    php artisan module:make Batch

---
+++ sandbox-laravel-modules/Modules/Batch/database/seeders/BatchDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Batch\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class BatchDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
---
+++ sandbox-laravel-modules/Modules/Batch/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('batch::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>Module: {!! config('batch.name') !!}</p>
+@endsection
